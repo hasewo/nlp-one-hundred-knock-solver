b7af3fa4725d5a868472c478b439b2ce56e0c988
hasewo <gastro2214@gmail.com>
2018-05-29 22:57:03 +0900

    [WIP]05 first commit

---
+++ 05.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def bigram(sentence, case):
+
+    if case == "word" or "char":
+        if case == "word":
+            split = sentence.split(" ")
+        elif case == "char":
+            split = sentence.replace(" ", "")
+        print(split)
+        bigram = ''
+        bigram_list = []
+
+        for i in range(len(split)-1):
+            bigram += split[i]
+            if case == "word":
+                bigram += ' ' + split[i+1]
+            else:
+                bigram += split[i+1]
+            bigram_list.insert(i, bigram)
+
+            bigram = ''
+
+        print(bigram_list)
+    else:
+        print('error')
+
+sentence = "I am an NLPer"
+bigram(sentence, "char")
