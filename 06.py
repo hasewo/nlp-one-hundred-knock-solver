25222e3ef0798c35b2d3f66dd2b2d9fa248e734b
hasewo <gastro2214@gmail.com>
2018-05-31 23:52:59 +0900

    06 commit

--- 06.py
+++ 06.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from func.bigram import bigram
+
+sentence1 = "paraparaparadise"
+sentence2 = "paragraph"
+
+X = set(bigram(sentence1, "char"))
+Y = set(bigram(sentence2, "char"))
+
+# 和集合
+union = X.union(Y)
+print(union)
+
+# 差集合
+diff = X.difference(Y)
+print(diff)
+# 積集合
+intersection = X.intersection(Y)
+print(intersection)
+
+# 'se'というbi-gramがXおよびYに含まれるかどうかを調べよ．
+print('se' in intersection)
